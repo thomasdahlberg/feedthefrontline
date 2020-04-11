074de221035cf23341fc664cd07cd33f3f5444c5
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-04-11 17:29:36 -0400

    "initializes views and routing for restaurant data objs"

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('test/', views.test, name="test"),
+    path('restaurants/', views.rest_index, name='rest_index'),
+    path('restaurants/', views.rest_profile, name='rest_profile'),
+    path('restaurants/<int:rest_id>/assoc_fac/<int:fac_id>/', views.assoc_fac, name='assoc_fac'),
+    path('restaurants/<int:rest_id>/rm_fac/<int:fac_id>', views.rm_fac, name='rm_fac'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Restaurant, Transaction, Facility
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-def test(request):
-    return render(request, 'test.html')
+# Restuarant Views (Visitor Visible)
+
+def rest_index(request):
+    restaurants = Restaurant.objects.all()
+    return render(request, 'restaurants/index.html', { 'restaurants' : restaurants })
+
+def rest_profile(request, rest_id):
+    restaurant = Restaurant.objects.get(id=rest_id)
+    return render(request, 'restaurants/detail.html', { 'restuarant': restaurant })
+
+# Restaurant Owner Views
+
+def assoc_fac(request, rest_id, fac_id):
+    pass
+
+def rm_fac(request, rest_id, fac_id):
+    pass
+
+def upload_logo(request, rest_id):
+    pass
+
+class RestCreate(CreateView):
+    model = Restaurant
+    fields = [
+        'restaurantName',
+        'phone',
+        'url',
+        'logo',
+        'aboutUs',
+        'mealCost',
+        'goal',
+    ]
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
