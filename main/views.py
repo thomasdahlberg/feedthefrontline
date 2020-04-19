0309f1be8be1a7e213d93ed0073c3f0822992995
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-04-19 14:41:37 -0700

    style/text updates

--- main/views.py
+++ main/views.py
@@ -42,12 +42,13 @@ def add_meals(request, restaurant_id):
 
 def create_transaction(request, restaurant_id):
     restaurant = Restaurant.objects.get(id=restaurant_id)
+    rest_facs = restaurant.facility_set.all()
     transaction = Transaction(restaurant=restaurant, mealNumber=int(request.GET['meal_number']), dollarAmount=request.GET['dollar_amount'], date=datetime.datetime.now())
     transaction.save()
     restaurant.mealsDonated += int(request.GET['meal_number'])
     restaurant.save()
     print(transaction)
-    context = {'restaurant': restaurant, 'transaction': transaction}
+    context = {'restaurant': restaurant, 'transaction': transaction, 'rest_facs': rest_facs}
     return render(request, 'thankyou.html', context)
 
 # Authorization and Registration
